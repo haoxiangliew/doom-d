45def02ae2c4ee6e5c359f482372853a3f4c1bb8
Hao Xiang Liew <haoxiangliew@gmail.com>
2020-11-03 23:39:37 -0500

    update email

--- config.el
+++ config.el
@@ -103,3 +103,8 @@
 ;; company-mode
 (setq company-minimum-prefix-length 1
       company-idle-delay 0.0)
+
+;; mu4e
+(setq mu4e-maildir (expand-file-name "~/mbsync"))
+(setq mu4e-get-mail-command "true")
+(setq mu4e-change-filenames-when-moving t)
